acf35b3b191e2a5f76f3fe701d2081afc81b9376
Bilal Arslan <bilarslan@windowslive.com>
2023-09-08 22:06:14 +0300

    project commit

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*
+//Single
+import { useRef } from 'react';
+import { View } from 'react-native';
+
+import ToastMessage from './components/ToastMessage';
+import Button from './components/Button';
 
 export default function App() {
+
+  const toastRef = useRef(null);
+
+  const handleShowToast = () => {
+    if (toastRef.current) {
+      toastRef.current.show();
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+      <ToastMessage
+        type="success"
+        text="This is a success toast message!"
+        description="This is description text"
+        timeout={2000}
+        ref={toastRef} />
+
+      <Button type="success" text="Success" onPress={handleShowToast} />
+
     </View>
   );
-}
+};
+*/
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//Multiple
+import { useRef, useState } from 'react';
+import { View } from 'react-native';
+
+import ToastMessage from './components/ToastMessage';
+import Button from './components/Button';
+
+export default function App() {
+
+  const [toastType, setToastType] = useState("success");
+  const toastRef = useRef(null);
+
+  const handleShowToast = () => {
+    if (toastRef.current) {
+      toastRef.current.show();
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+      <ToastMessage
+        type={toastType}
+        text="Lorem Ipsum Text"
+        description="Lorem Ipsum Description"
+        ref={toastRef} />
+
+      <Button type="success" text="Success" onPress={() => { setToastType("success"); handleShowToast(); }} />
+      <Button type="danger" text="Danger" onPress={() => { setToastType("danger"); handleShowToast(); }} />
+      <Button type="info" text="Info" onPress={() => { setToastType("info"); handleShowToast(); }} />
+      <Button type="warning" text="Warning" onPress={() => { setToastType("warning"); handleShowToast(); }} />
+
+    </View>
+  );
+};
---
+++ README.md
@@ -0,0 +1,24 @@
+## Toast Messages in React Native
+![Screenshot](screenshot.png)
+
+### 📖Read blog post
+https://reactnativespace.com/tutorials/toast-messages-in-react-native
+
+### 📘 Installation
+Clone the repo to your directory and install the packages through npm using:
+```
+npm install
+```
+
+### 🔬 Run
+Run the project with Metro Bundler
+```
+npx expo start
+```
+
+### 👇 Follow me;
+- https://reactnativespace.com
+- https://github.com/reactnativespace
+- https://twitter.com/reactnativespc
+- https://instagram.com/reactnativespace
+- https://www.tiktok.com/@reactnativespace
