92029f37c6f71523956bd3e542bb4d6f93f1afe4
Pasha <suprunchuk@internet.ru>
2023-10-26 02:41:40 +0300

    Fixed a bug with toast disappearing
    I fixed two bugs.
    1) When I used this ToastMessage in other parts of my application via import, this notification appeared and did NOT disappear. It was constantly displayed on the screen. I fixed this using useEffect.
    2) By using this notification in different parts of the application, it could be displayed incorrectly and move to the left side of the screen, even if the display was centered in the parent View. I added Dimensions.get and calculated the screen width and centered the toast depending on the screen. This is very cool.
    I also added Easing to the animation to improve the smoothness of the animation.

--- components/ToastMessage.jsx
+++ components/ToastMessage.jsx
@@ -1,77 +1,88 @@
-import { useState, forwardRef, useImperativeHandle } from 'react';
-import { View, Text, } from 'react-native';
-import Animated, { FadeInUp, FadeOutUp } from 'react-native-reanimated';
-import { FontAwesome5 } from '@expo/vector-icons';
+import React, { useState, forwardRef, useImperativeHandle, useEffect } from "react";
+import { View, Text, Dimensions } from "react-native";
+import Animated, { FadeInUp, FadeOutUp, Easing } from "react-native-reanimated";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-const ToastMessage = forwardRef(({ type, text, description, timeout = 1000 }, ref) => {
-
+const ToastMessage = forwardRef(({ type, text, description, timeout = 1800 }, ref) => {
     const [isVisible, setIsVisible] = useState(false);
 
     const TOAST_TYPE = {
         success: {
-            backgroundColor: '#2ecc71',
-            icon: 'check-circle'
+            backgroundColor: "#2ecc71",
+            icon: "check-circle",
         },
         danger: {
-            backgroundColor: '#e74c3c',
-            icon: 'exclamation-circle'
+            backgroundColor: "#e74c3c",
+            icon: "exclamation-circle",
         },
         info: {
-            backgroundColor: '#3498db',
-            icon: 'info-circle'
+            backgroundColor: "#3498db",
+            icon: "info-circle",
         },
         warning: {
-            backgroundColor: '#f39c12',
-            icon: 'exclamation-triangle'
-        }
-    }
+            backgroundColor: "#f39c12",
+            icon: "exclamation-triangle",
+        },
+    };
 
     const showToast = () => {
         setIsVisible(true);
-        const timer = setTimeout(() => {
-            setIsVisible(false);
-            clearTimeout(timer);
-        }, timeout);
     };
 
     useImperativeHandle(ref, () => ({
-        show: showToast
+        show: showToast,
     }));
 
+    useEffect(() => {
+        if (isVisible) {
+            const timer = setTimeout(() => {
+                setIsVisible(false);
+                clearTimeout(timer);
+            }, timeout);
+        }
+    }, [isVisible, timeout]);
 
     const backgroundColor = TOAST_TYPE[type].backgroundColor;
     const icon = TOAST_TYPE[type].icon;
-
+    const { width } = Dimensions.get("window");
     return (
         <>
-            {isVisible && <Animated.View style={{
-                position: 'absolute',
-                top: 50,
-                width: '90%', height: 100, backgroundColor: backgroundColor, borderRadius: 10,
-                padding: 12,
-                flexDirection: 'row',
-                alignItems: 'center',
-                shadowColor: "#000",
-                shadowOffset: {
-                    width: 0,
-                    height: 2,
-                },
-                shadowOpacity: 0.25,
-                shadowRadius: 3.84,
-                elevation: 5,
-            }}
-                entering={FadeInUp.delay(200)}
-                exiting={FadeOutUp}
-            >
-                <FontAwesome5 name={icon} size={40} color="#FFF" />
+            {isVisible && (
+                <Animated.View
+                    style={{
+                        position: "absolute",
+                        top: 50,
+                        width: "90%",
+                        left: (width - width * 0.9) / 2,
+                        height: 80,
+                        backgroundColor: backgroundColor,
+                        borderRadius: 10,
+                        padding: 12,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        shadowColor: "#000",
+                        shadowOffset: {
+                            width: 0,
+                            height: 2,
+                        },
+                        shadowOpacity: 0.25,
+                        shadowRadius: 3.84,
+                        elevation: 5,
+                        zIndex: 99999999999,
+                    }}
+                    entering={FadeInUp.delay(200, Easing.bezier(0.25, 0.1, 0.25, 1))}
+                    exiting={FadeOutUp}
+                >
+                    <FontAwesome5 name={icon} size={40} color="#FFF" />
 
-                <View style={{ marginLeft: 12 }}>
-                    <Text style={{ fontSize: 18, fontWeight: '600', color: '#FFF' }}>{text}</Text>
-                    <Text style={{ fontSize: 16, fontWeight: '400', color: '#FFF' }}>{description}</Text>
-                </View>
-            </Animated.View >}
+                    <View style={{ marginLeft: 12 }}>
+                        <Text style={{ fontSize: 18, fontWeight: "600", color: "#FFF" }}>{text}</Text>
+                        <Text style={{ fontSize: 16, fontWeight: "400", color: "#FFF" }}>{description}</Text>
+                    </View>
+                </Animated.View>
+            )}
         </>
-    )
+    );
 });
 
 export default ToastMessage;
